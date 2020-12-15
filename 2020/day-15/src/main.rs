cbc2d9a77d34674291c2966755bdd557ddc52925
fuyu <fuyu@matsuri.io>
2020-12-14 22:38:56 -0800

    Cleaned up day 15

--- 2020/day-15/src/main.rs
+++ 2020/day-15/src/main.rs
@@ -7,28 +7,18 @@ static _INPUT: &str = "6,19,0,5,7,13,1";
 fn main() {
     let game = _INPUT
         .split(',')
-        .map(|number| number.parse::<u32>().unwrap())
+        .map(|number| number.parse::<usize>().unwrap())
         .enumerate()
         .map(|(i, val)| (val, i));
 
-    let mut prev = game.clone().last().unwrap().0;
+    let prev = game.clone().last().unwrap().0;
 
-    let mut game = game.collect::<HashMap<u32, usize>>();
+    let mut game = game.collect::<HashMap<usize, usize>>();
 
-    let size = 30000000;
-    (game.len()..size).into_iter().for_each(|i| {
-        let last = game.get_mut(&prev);
-
-        if let Some(last) = last {
-            let pos = *last;
-            prev = (i - 1 - pos) as u32;
-
-            *last = i - 1;
-        } else {
-            game.insert(prev, i - 1);
-            prev = 0;
-        }
+    let size = 30_000_000;
+    let result = (game.len()..size).fold(prev, |prev, i| {
+        i - 1 - game.insert(prev, i - 1).unwrap_or(i - 1)
     });
 
-    println!("{:#?}", prev);
+    println!("{:#?}", result);
 }
