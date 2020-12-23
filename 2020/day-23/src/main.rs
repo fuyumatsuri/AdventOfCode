0b76f9ca4b4769f3a393f8230590862615284624
fuyu <fuyu@matsuri.io>
2020-12-22 23:42:58 -0800

    Only need single linked list

--- 2020/day-23/src/main.rs
+++ 2020/day-23/src/main.rs
@@ -9,23 +9,21 @@ fn main() {
 
     cups.extend(cups.len() + 1..=1_000_000);
 
-    let mut linked_list = vec![(0, 0); cups.len() + 1];
+    let mut linked_list = vec![0; cups.len() + 1];
 
-    for window in cups.windows(3) {
-        linked_list[window[1]] = (window[0], window[2]);
+    for window in cups.windows(2) {
+        linked_list[window[0]] = window[1];
     }
-    linked_list[cups[0]] = (cups[cups.len() - 1], cups[1]);
-    linked_list[cups[cups.len() - 1]] = (cups[cups.len() - 2], cups[0]);
+    linked_list[cups[cups.len() - 1]] = cups[0];
 
     let mut current = cups[0];
     for _ in 0..10_000_000 {
-        let a = linked_list[current].1;
-        let b = linked_list[a].1;
-        let c = linked_list[b].1;
+        let a = linked_list[current];
+        let b = linked_list[a];
+        let c = linked_list[b];
 
-        let new_next = linked_list[c].1;
-        linked_list[current].1 = new_next;
-        linked_list[new_next].0 = current;
+        let new_next = linked_list[c];
+        linked_list[current] = new_next;
 
         let mut dest = current - 1;
 
@@ -42,15 +40,13 @@ fn main() {
             dest -= 1;
         }
 
-        let after_dest = linked_list[dest].1;
-        linked_list[dest].1 = a;
-        linked_list[c].1 = after_dest;
-
-        linked_list[after_dest].0 = c;
+        let after_dest = linked_list[dest];
+        linked_list[dest] = a;
+        linked_list[c] = after_dest;
     }
 
-    let next = linked_list[1].1;
-    let next2 = linked_list[next].1;
+    let next = linked_list[1];
+    let next2 = linked_list[next];
 
     println!("{} * {} = {}", next, next2, next * next2);
 }
